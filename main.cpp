a30f5ec5207313602362071fb3a607458678e6b3
vali2301 <valicirstea23@gmail.com>
2025-10-28 15:42:11 +0200

    functii globale puse in class

--- main.cpp
+++ main.cpp
@@ -68,107 +68,8 @@ class Echipa {
     }
 };
 
-    void simulareVreme();
-
-
-class Cursa {
-    private:
-    std::string locatie;
-    std::vector<Echipa> echipe;
-    std::vector<int> puncteF1 = {25, 18, 15, 12, 10 ,8 ,6, 4, 2, 1};
-    public:
-    Cursa(std::string l, std::vector<Echipa> e) : locatie(l), echipe(e) {}
-
-    int punctePilotCursa(const std::string& pilotAles, std::map<std::string, int>& scoruri) {
-        std::vector<Pilot> totiPiloti;
-        for (auto &echipa : echipe)
-            for (auto &p : echipa.getPiloti())
-                totiPiloti.push_back(p);
-
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::shuffle(totiPiloti.begin(), totiPiloti.end(), gen);
-
-        std::cout << "\nCursa din " << locatie << " a inceput!\n\n";
-        simulareVreme();
-
-        std::uniform_int_distribution<> distAbandon(0, 9); //10%
-        std::vector<std::string> abandonuri;
-
-        for (auto it = totiPiloti.begin(); it != totiPiloti.end();) {
-            if (distAbandon(gen) == 0) {
-                abandonuri.push_back(it->getNume());
-                it = totiPiloti.erase(it);
-            } else {
-                ++it;
-            }
-        }
-
-
-        std::cout << "Clasament final pentru " << locatie << ":\n";
-
-        std::uniform_real_distribution<> distTimp(75.0, 80.0);
-        std::vector<double> timpi;
-        for (size_t i = 0; i < totiPiloti.size(); ++i)
-            timpi.push_back(distTimp(gen) + i * 0.5);
-
-
-        std::sort(timpi.begin(), timpi.end());
-        double timpPrimul = timpi[0];
-
-        int punctePilot = 0;
-        std::vector<std::pair<std::string, double>> rezultate;
-
-        for (size_t i = 0; i < totiPiloti.size(); ++i) {
-            int puncte = (i < puncteF1.size()) ? puncteF1[i] : 0;
-            totiPiloti[i].adaugaPuncte(puncte);
-            scoruri[totiPiloti[i].getNume()] += puncte;
-            rezultate.push_back({totiPiloti[i].getNume(), timpi[i]});
-            if (totiPiloti[i].getNume() == pilotAles)
-                punctePilot = puncte;
-        }
-
-
-        int randomBonus = gen() % totiPiloti.size();
-        std::string pilotBonus = totiPiloti[randomBonus].getNume();
-        scoruri[pilotBonus] += 1;
-
-        for (size_t i = 0; i < rezultate.size(); ++i) {
-            double diferenta = rezultate[i].second - timpPrimul;
-            std::cout << i + 1 << ". " << rezultate[i].first
-                      << " - " << puncteF1[std::min(i, puncteF1.size() - 1)]
-                      << " pct | Timp: " << rezultate[i].second
-                      << " min | +" << diferenta << "s\n";
-        }
-
-        if (!abandonuri.empty()) {
-            std::cout << "\nAbandonuri (DNF):\n";
-            for (auto &nume : abandonuri)
-                std::cout << "- " << nume << " (DNF)\n";
-        }
-
-
-
-        std::cout << "\nCel mai rapid tur: " << pilotBonus << " (+1 punct bonus)\n";
-
-        auto it = std::find_if(rezultate.begin(), rezultate.end(),
-                               [&](auto &x){ return x.first == pilotAles; });
-        if (it != rezultate.end())
-            std::cout << "\nPilotul tau (" << pilotAles << ") a terminat pe locul "
-                      << (it - rezultate.begin() + 1)
-                      << " si a obtinut " << punctePilot << " puncte.\n";
-
-        return punctePilot;
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Cursa& c) {
-        os << "Cursa: " << c.locatie
-           << " (cu " << c.echipe.size() << " echipe participante)";
-        return os;
-    }
-
-    };
-
+    class Campionat{
+public:
 
 void afiseazaClasamentEchipe(const std::map<std::string, int> &scoruri) {
     std::map<std::string, std::string> pilotEchipa = {
@@ -205,9 +106,9 @@ void afiseazaClasamentEchipe(const std::map<std::string, int> &scoruri) {
         std::cout << "Alege o optiune: ";
         int opt;
         while (true) {
-            if (std::cin >> opt && opt >= 1 && opt <= 6)
+            if (std::cin >> opt && opt >= 1 && opt <= 7)
                 return opt;
-            std::cout << "Optiune invalida! Alege 1 - 6: ";
+            std::cout << "Optiune invalida! Alege 1 - 7: ";
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
@@ -342,10 +243,113 @@ void simulareSafetyCar() {
 }
 
 
+};
+
+class Cursa {
+    private:
+    std::string locatie;
+    std::vector<Echipa> echipe;
+    std::vector<int> puncteF1 = {25, 18, 15, 12, 10 ,8 ,6, 4, 2, 1};
+    Campionat &campionat;
+    public:
+    Cursa(std::string l, std::vector<Echipa> e, Campionat &c)
+    : locatie(l), echipe(e), campionat(c) {}
+
+    int punctePilotCursa(const std::string& pilotAles, std::map<std::string, int>& scoruri) {
+        std::vector<Pilot> totiPiloti;
+        for (auto &echipa : echipe)
+            for (auto &p : echipa.getPiloti())
+                totiPiloti.push_back(p);
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::shuffle(totiPiloti.begin(), totiPiloti.end(), gen);
+
+        std::cout << "\nCursa din " << locatie << " a inceput!\n\n";
+        campionat.simulareVreme();
+
+        std::uniform_int_distribution<> distAbandon(0, 9); //10%
+        std::vector<std::string> abandonuri;
+
+        for (auto it = totiPiloti.begin(); it != totiPiloti.end();) {
+            if (distAbandon(gen) == 0) {
+                abandonuri.push_back(it->getNume());
+                it = totiPiloti.erase(it);
+            } else {
+                ++it;
+            }
+        }
+
+
+        std::cout << "Clasament final pentru " << locatie << ":\n";
+
+        std::uniform_real_distribution<> distTimp(75.0, 80.0);
+        std::vector<double> timpi;
+        for (size_t i = 0; i < totiPiloti.size(); ++i)
+            timpi.push_back(distTimp(gen) + i * 0.5);
+
+
+        std::sort(timpi.begin(), timpi.end());
+        double timpPrimul = timpi[0];
+
+        int punctePilot = 0;
+        std::vector<std::pair<std::string, double>> rezultate;
+
+        for (size_t i = 0; i < totiPiloti.size(); ++i) {
+            int puncte = (i < puncteF1.size()) ? puncteF1[i] : 0;
+            totiPiloti[i].adaugaPuncte(puncte);
+            scoruri[totiPiloti[i].getNume()] += puncte;
+            rezultate.push_back({totiPiloti[i].getNume(), timpi[i]});
+            if (totiPiloti[i].getNume() == pilotAles)
+                punctePilot = puncte;
+        }
+
+
+        int randomBonus = gen() % totiPiloti.size();
+        std::string pilotBonus = totiPiloti[randomBonus].getNume();
+        scoruri[pilotBonus] += 1;
+
+        for (size_t i = 0; i < rezultate.size(); ++i) {
+            double diferenta = rezultate[i].second - timpPrimul;
+            std::cout << i + 1 << ". " << rezultate[i].first
+                      << " - " << puncteF1[std::min(i, puncteF1.size() - 1)]
+                      << " pct | Timp: " << rezultate[i].second
+                      << " min | +" << diferenta << "s\n";
+        }
+
+        if (!abandonuri.empty()) {
+            std::cout << "\nAbandonuri (DNF):\n";
+            for (auto &nume : abandonuri)
+                std::cout << "- " << nume << " (DNF)\n";
+        }
+
+
+
+        std::cout << "\nCel mai rapid tur: " << pilotBonus << " (+1 punct bonus)\n";
+
+        auto it = std::find_if(rezultate.begin(), rezultate.end(),
+                               [&](auto &x){ return x.first == pilotAles; });
+        if (it != rezultate.end())
+            std::cout << "\nPilotul tau (" << pilotAles << ") a terminat pe locul "
+                      << (it - rezultate.begin() + 1)
+                      << " si a obtinut " << punctePilot << " puncte.\n";
+
+        return punctePilot;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Cursa& c) {
+        os << "Cursa: " << c.locatie
+           << " (cu " << c.echipe.size() << " echipe participante)";
+        return os;
+    }
+
+    };
+
 
 
 
 int main() {
+       Campionat campionat;
     std::vector<Echipa> echipe ={
 
         Echipa("Red Bull" , {"Verstappen" , "Tsunoda"}),
@@ -388,41 +392,41 @@ std:: string pilotAles = piloti[alegerePilot].getNume();
     std::cout<<"\n Ai ales sa concurezi cu "<<pilotAles<<"!\n";
 
         for (size_t i = 0; i < circuite.size(); ++i) {
-            Cursa cursa(circuite[i], echipe);
+            Cursa cursa(circuite[i], echipe, campionat);
             int puncteCursa = cursa.punctePilotCursa(pilotAles, scoruri);
             puncteTotale += puncteCursa;
 
             if (i < circuite.size() - 1) {
                 int opt;
                 do {
-                    opt = meniuCampionat();
+                    opt = campionat.meniuCampionat();
                     if (opt == 2) {
-                        afiseazaClasamentGeneral(scoruri);
-                        afiseazaClasamentEchipe(scoruri);
+                        campionat.afiseazaClasamentGeneral(scoruri);
+                        campionat.afiseazaClasamentEchipe(scoruri);
                     }
                     else if (opt == 3) {
-                        afisareRegulament();
+                        campionat.afisareRegulament();
                     }
                     else if (opt == 4){
-                        afisareCircuitInfo();
+                        campionat.afisareCircuitInfo();
                     }
                     else if (opt == 5){
-                        simularePitStop();
+                        campionat.simularePitStop();
                     }
                        else if (opt == 6){
-                        simulareSafetyCar();
+                        campionat.simulareSafetyCar();
                     }
                     else if (opt == 7) {
                         std::cout << "\nCampionatul s-a incheiat mai devreme!\n";
-                        afiseazaClasamentGeneral(scoruri);
+                        campionat.afiseazaClasamentGeneral(scoruri);
                         return 0;
                     }
                 } while (opt != 1);
             }
         }
 
-        afiseazaClasamentGeneral(scoruri);
-    afiseazaClasamentEchipe(scoruri);
+        campionat.afiseazaClasamentGeneral(scoruri);
+    campionat.afiseazaClasamentEchipe(scoruri);
 
 
     std::vector<std::pair<std::string, int>> clasamentFinal(scoruri.begin(), scoruri.end());

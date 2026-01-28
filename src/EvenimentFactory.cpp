1d536a988f45a39ee0faf88c3e7a70c59acec7f3
vali2301 <valicirstea23@gmail.com>
2026-01-28 17:06:13 +0200

    t3

--- src/EvenimentFactory.cpp
+++ src/EvenimentFactory.cpp
@@ -12,10 +12,10 @@ std::unique_ptr<Eveniment> FactoryCalificari::creeazaEveniment(const std::string
     return std::make_unique<Calificari>(locatie, camp, echipe);
 }
 
-std::unique_ptr<Eveniment> FactorySprint::creeazaEveniment(const std::string& locatie, const std::vector<Echipa>& echipe, Campionat& camp) const {
+std::unique_ptr<Eveniment> FactorySprint::creeazaEveniment(const std::string& locatie, const std::vector<Echipa>& echipe, Campionat& /*camp*/) const {
     return std::make_unique<SprintRace>(locatie, echipe, 5); // 5 este agresivitatea default
 }
 
-std::unique_ptr<Eveniment> FactoryRecunoastere::creeazaEveniment(const std::string& locatie, const std::vector<Echipa>& echipe, Campionat& camp) const {
+std::unique_ptr<Eveniment> FactoryRecunoastere::creeazaEveniment(const std::string& locatie, const std::vector<Echipa>& echipe, Campionat& /*camp*/) const {
     return std::make_unique<RecunoastereCircuit>(locatie, "Sesiune de recunoastere", echipe);
 }
